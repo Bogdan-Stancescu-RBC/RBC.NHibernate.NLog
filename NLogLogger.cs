0e060572a219cb903f58d5ad1b682c9527f992ae
Bogdan Stancescu <bogdan.stancescu@rbc.com.ro>
2020-12-13 16:36:40 +0200

    Add project files.

---
+++ NLogLogger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using NHibernate;
+using NLog;
+
+namespace RBC.NHibernate.NLog
+{
+    /// <summary>
+    /// This class actually links NHibernate to NLog.
+    /// You shouldn't ever need to instantiate one of these directly.
+    /// </summary>
+    public class NLogLogger : INHibernateLogger
+    {
+        private readonly Logger _logger;
+
+        private static Dictionary<NHibernateLogLevel, LogLevel> levelMapper = new Dictionary<NHibernateLogLevel, LogLevel>()
+        {
+            { NHibernateLogLevel.Trace, LogLevel.Trace },
+            { NHibernateLogLevel.Debug, LogLevel.Debug },
+            { NHibernateLogLevel.Info, LogLevel.Info },
+            { NHibernateLogLevel.Warn, LogLevel.Warn },
+            { NHibernateLogLevel.Error, LogLevel.Error },
+            { NHibernateLogLevel.Fatal, LogLevel.Fatal },
+            { NHibernateLogLevel.None, LogLevel.Off },
+        };
+
+        public NLogLogger(string logger)
+        {
+            _logger = LogManager.GetLogger(logger);
+        }
+
+        public bool IsEnabled(NHibernateLogLevel logLevel)
+        {
+            return _logger.IsEnabled(levelMapper[logLevel]);
+        }
+
+        public void Log(NHibernateLogLevel logLevel, NHibernateLogValues state, Exception exception)
+        {
+            _logger.Log(levelMapper[logLevel], exception, state.Format, state.Args);
+        }
+    }
+
+}
---
+++ NLogLoggerFactory.cs
@@ -0,0 +1,23 @@
+﻿using NHibernate;
+
+namespace RBC.NHibernate.NLog
+{
+    /// <summary>
+    /// The main logger factory; this is the only class you need to use explicitly.
+    /// Just call static method NHibernateLogger.SetLoggersFactory() with a
+    /// NLogLoggerFactory instance during NHibernate initialization, and you're done:
+    /// NHibernateLogger.SetLoggersFactory(new NLogLoggerFactory());
+    /// </summary>
+    public class NLogLoggerFactory : INHibernateLoggerFactory
+    {
+        INHibernateLogger INHibernateLoggerFactory.LoggerFor(string keyName)
+        {
+            return new NLogLogger(keyName);
+        }
+
+        INHibernateLogger INHibernateLoggerFactory.LoggerFor(System.Type type)
+        {
+            return new NLogLogger(type.FullName);
+        }
+    }
+}
